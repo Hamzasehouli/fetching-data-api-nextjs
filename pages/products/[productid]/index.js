68d5ccc23ed5281a28a9d9436805f14d5dabe24c
Hamzasehouli <sehoulihamza@yahoo.com>
2022-04-27 15:50:48 +0000

    Added getServerSideProps() function in the [productid] page

--- pages/products/[productid]/index.js
+++ pages/products/[productid]/index.js
@@ -4,20 +4,35 @@ const Product = function (props) {
   return <div>product</div>;
 };
 
-export async function getStaticPaths() {
-  return {
-    paths: [
-      {
-        params: {
-          productid: "1",
-        },
-      },
-    ],
-    fallback: false, // false or 'blocking'
-  };
-}
+// export async function getStaticPaths() {
+//   return {
+//     paths: [
+//       {
+//         params: {
+//           productid: "1",
+//         },
+//       },
+//     ],
+//     fallback: false, // false or 'blocking'
+//   };
+// }
 
-export async function getStaticProps(context) {
+// export async function getStaticProps(context) {
+//   console.log(context);
+//   const param = context.params.productid;
+//   const res = await fetch(
+//     `https://jsonplaceholder.typicode.com/todos/${param}`
+//   );
+
+//   const data = await res.json();
+
+//   return {
+//     props: {
+//       data,
+//     },
+//   };
+// }
+export async function getServerSideProps(context) {
   console.log(context);
   const param = context.params.productid;
   const res = await fetch(
